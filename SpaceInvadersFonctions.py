0fe600395107fc470134a0bf26e46da1cf21ebe2
MarieArnaud18 <72148986+MarieArnaud18@users.noreply.github.com>
2021-01-17 19:58:43 +0100

    last commit

--- SpaceInvadersFonctions.py
+++ SpaceInvadersFonctions.py
@@ -1,60 +1,77 @@
 from tkinter import *
-DXA = 1.5
-DYA = 0
-j = 0
-i = -1
-class Alien:
+
+DXA = 1.5  # initialisation des paramètres globaux
+DYA = 20
+arretdujeu = False
+aliendetruit = 0
+
+class Alien:  # création de la classe Alien
     def __init__(self, x, y, canyoushot, indice, jeu, fenetre, rayonA, alientag):
         self.x = x
         self.y = y
-        self.canyoushot = canyoushot
+        self.canyoushot = canyoushot # paramêtre aléatoire qui devait servir à déterminer si l'alien était autorisé à tirer (pas développé)
         self.indice = indice
         self.jeu = jeu
         self.fenetre = fenetre
         self.rayonA = rayonA
         self.alientag = alientag
         self.vaisseau = None
-        self.alientag = self.jeu.create_oval (x-rayonA, y-rayonA, x+rayonA, y+rayonA, outline = 'black', fill = 'pink')
-        self.vivant = True
+        self.alientag = self.jeu.create_oval (x-rayonA, y-rayonA, x+rayonA, y+rayonA, outline = 'black', fill = 'pink') # dessin de l'alien sur le canvas et récupération de son identifiant
     
-    def deplacement ( self, rayonA):
-        [x1, y1, x2, y2] = self.jeu.bbox(self.alientag)
-        global DXA,DYA,j,i
-        if x2 > 1000 :
+    def deplacement_horizontal ( self):  # fonction permettant le déplacement horizontal des aliens
+        [x1, y1, x2, y2] = self.jeu.bbox(self.alientag) # récupération des coordonnées de l'alien 
+        global DXA, arretdujeu
+        if x2 > 1000 : # condition de contact sur le bord droit du canvas
             DXA = -1.5
-        if x1 < 0 :
-            j += 1
-            print (i,j,'premier')
+        if x1 < 0 : # condition de contact sur le bord gauche du canvas
             DXA = 1.5
-            if i != j :
-                i +=1
-                DYA = 175/6
-                print(i,j,'second')
-            if i == j :
-                DYA = 0
-
-        self.jeu.move (self.alientag, DXA, DYA) 
-        self.fenetre.after ( 20, lambda : self.deplacement( rayonA))
+        self.jeu.move (self.alientag, DXA, 0) # déclenche le déplacement de l'alien 
+        if not arretdujeu :  # condition de non arrêt du jeu 
+            self.fenetre.after ( 29, lambda : self.deplacement_horizontal()) # bouclage de la fonction sur un temps 
 
 
+    def deplacement_vertical (self, hauteur, largeur, defaite, Defaite): # fonction permettant le déplacement vertical des aliens
+        global DYA, arretdujeu
+        [x1, y1, x2, y2] = self.jeu.bbox(self.alientag)
+        if x1 < 0 : 
+            liste_y1 = self.jeu.find_overlapping (x1, y1, x2, y1+(2/12)*hauteur+self.rayonA) # récupère la liste des identifiants des éléments se situant dans le rectangle définit
+            liste_y2 = self.jeu.find_overlapping (x1+(1/7)*largeur, y1, x2+(1/7)*largeur, y1+(2/12)*hauteur+self.rayonA) # idem
+            liste_y3 = self.jeu.find_overlapping (x1+(2/7)*largeur, y1, x2+(2/7)*largeur, y1+(2/12)*hauteur+self.rayonA)
+            liste_y4 = self.jeu.find_overlapping (x1+(3/7)*largeur, y1, x2+(3/7)*largeur, y1+(2/12)*hauteur+self.rayonA)
+            liste_y5 = self.jeu.find_overlapping (x1+(4/7)*largeur, y1, x2+(4/7)*largeur, y1+(2/12)*hauteur+self.rayonA)
+            liste_y6 = self.jeu.find_overlapping (x1+(5/7)*largeur, y1, x2+(5/7)*largeur, y1+(2/12)*hauteur+self.rayonA)
+            liste_y = liste_y1 + liste_y2 + liste_y3 + liste_y4 + liste_y5 + liste_y6 # concaténation des listes
+            for i in liste_y : 
+                if i < 19: # vérifie que l'élément est un alien  
+                    self.jeu.move (i, 0, DYA)
+        if y2 > hauteur : # condition de défaite (les aliens touchent le bas du canvas)
+            self.jeu.delete(19) # destruction du vaisseau
+            Defaite.focus_set()
+            defaite.set("Perdu...") # affichage du message de defaite 
+            arretdujeu = True # variable permettant l'arrêt du jeu 
 
-def PositionsInit ( largeur, hauteur, X, Y, jeu, fenetre, rayonA):
-    distancehorizontale = largeur/7
-    distanceverticale = hauteur/(3*4)
-    n = 0
+        if not arretdujeu:
+            self.fenetre.after ( 30, lambda : self.deplacement_vertical(hauteur, largeur, defaite, Defaite))
+    
+
+
+
+def PositionsInit ( largeur, hauteur, X, Y, jeu, fenetre, rayonA): # fonction définissant la position initiale de chaque alien
+    distancehorizontale = largeur/7  # définition des écarts verticaux entre aliens
+    distanceverticale = hauteur/(3*4) # définition des écarts horizontaux entre aliens
+    n = 0 
     A_liste = []
-    for i in range (1,7):
-        for j in range (1,4):
-            X.append (i*distancehorizontale)
+    for i in range (1,7): # boucle sur les colonnes d'aliens
+        for j in range (1,4): # boucle sur les lignes d'aliens
+            X.append (i*distancehorizontale) # definition des coordonnées des aliens 
             Y.append (j*distanceverticale)
-            A_liste.append(Alien(X[n], Y[n], False, n, jeu, fenetre, rayonA, 0))
-            
+            A_liste.append(Alien(X[n], Y[n], False, n, jeu, fenetre, rayonA, 0)) # création des objets alien et les ajoute dans la liste 
             n = n+1
     return(X,Y, A_liste)
 
 
 
-class Vaisseau:
+class Vaisseau: # création de la classe Vaisseau
     def __init__(self, jeu, fenetre, rayonV, rayonM, A_liste):
         self.jeu = jeu
         self.fenetre = fenetre
@@ -63,47 +80,51 @@ class Vaisseau:
         self.rayonM = rayonM
         self.A_liste = A_liste
 
-    def deplacement (self, DXV, DYM, event):
+    def deplacement (self, DXV, DYM, event, defaite, Defaite): # fonction permettant les actions du Vaisseau en fonction du clavier
         [x1, y1, x2, y2] = self.jeu.bbox(self.vaisseautag)
-        touche = event.keysym
-        if touche == "d":
-            if x2 > 1000:
-                DXV = 0
-            else: 
-                DXV = 8
+        touche = event.keysym # récupère le nom de l'entrée clavier
+        if touche == "d": # condition d'appui sur la touche "d"
+            if x2 > 1000: # si le vaisseau touche déjà le bord droit du canvas
+                DXV = 0 # pas de déplacement à droite autorisé
+            else: # sinon 
+                DXV = 8 # déplacement à droite autorisé
             self.jeu.move (self.vaisseautag, DXV, 0)
-        if touche == "q":
-            if x1 < 0 :
+        if touche == "q": # condition d'appui sur la touche "q"
+            if x1 < 0 : # si le vaisseau touche déjà le bord gauche du canvas
                 DXV = 0
-            else: 
+            else:
                 DXV = -8
             self.jeu.move (self.vaisseautag, DXV, 0)
-        if touche == "space": 
-            self.TireMissile (DYM)
-    def PositionInitV (self, rayonV):
-        self.vaisseautag = self.jeu.create_oval (500-self.rayonV, 11*700/12-self.rayonV, 500+self.rayonV, 11*700/12+self.rayonV, outline = 'black', fill = 'blue')
+        if touche == "space": # condition d'appui sur la touche "espace"
+            self.TireMissile (DYM, defaite, Defaite) # appel de la fonction qui permet de tirer un missile 
+    
+    def PositionInitV (self, rayonV): # création du vaisseau sur le canvas et récupération de son identifiant
+        self.vaisseautag = self.jeu.create_oval (500-self.rayonV, 11*700/12-self.rayonV, 500+self.rayonV, 11*700/12+self.rayonV, outline = 'black', fill = 'blue') # dessin du vaisseau sur le canvas
 
-    def TireMissile(self, DYM):
+    def TireMissile(self, DYM, defaite, Defaite): # fonction permattant de tirer un missile avec le vaisseau
         [x1, y1, x2, y2] = self.jeu.bbox(self.vaisseautag)
-        x = (x1+x2)/2
-        M = self.jeu.create_oval (x-self.rayonM, y1-self.rayonM, x+self.rayonM, y1+self.rayonM, outline = 'black', fill = 'blue')
-        self.DeplacementMissile (DYM, M)
+        x = (x1+x2)/2 # calcul de la position intiale du centre du missile définie grâce à la position du vaisseau (missile qui par du milieu supérieur du vaisseau)
+        M = self.jeu.create_oval (x-self.rayonM, y1-self.rayonM, x+self.rayonM, y1+self.rayonM, outline = 'black', fill = 'blue') # dessin du missile sur le canvas
+        self.DeplacementMissile (DYM, M, defaite, Defaite)
 
-    def DeplacementMissile(self, DYM, M):
+    def DeplacementMissile(self, DYM, M, defaite, Defaite): # fonction gérant les déplacements du missile 
+        global arretdujeu, aliendetruit
         [x1, y1, x2, y2] = self.jeu.bbox(M)
-        if y1 < 0:
-            self.jeu.delete(M)
-            print(M)
+        if y1 < 0: # si le missile atteint le bord supérieur du canvas
+            self.jeu.delete(M) # suppression du missile
             return
-        liste = self.jeu.find_overlapping (x1, y1, x2, y2)
-        print(liste)
+        liste = self.jeu.find_overlapping (x1, y1, x2, y2) # récupération des identifiants des éléments touchant le missile (y compris le missile)
         for i in liste : 
-            print(i)
-            print(self.jeu.find_withtag (M))
-            if i != 19 and i != self.jeu.find_withtag (M)[0] :
-                print ("Hello")  
-                self.jeu.delete(i)
-                self.jeu.delete(self.jeu.find_withtag (M)[0])
+            if i != 19 and i != self.jeu.find_withtag (M)[0] :  # si l'élément n'est pas le vaisseau ni le missile (donc si c'est un alien)
+                self.jeu.delete(i) # suppression de l'alien touché
+                self.jeu.delete(self.jeu.find_withtag (M)[0]) # suppression du missile qui a touché 
+                aliendetruit += 1 # compte le nombre d'aliens détruits
         else:
-            self.jeu.move (M, 0, DYM) 
-        self.fenetre.after (20, lambda : self.DeplacementMissile( DYM, M))
+            self.jeu.move (M, 0, DYM) # déplacement vertical du missile 
+        if aliendetruit == 18 : # condition de victoire ( tous les aliens ont été détruits)
+            arretdujeu = True
+            Defaite.focus_set()
+            defaite.set("Bien joué !") # affichage du message de victoire
+        if not arretdujeu:
+            self.fenetre.after (20, lambda : self.DeplacementMissile( DYM, M, defaite, Defaite))
+
--- SpaceInvadersInterface.py
+++ SpaceInvadersInterface.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import SpaceInvadersFonctions as SIF
 
-score = 1000
+score = 0000    #initialisation des paramètres 
 largeur = 1000
 hauteur = 700
 X = []
@@ -12,44 +12,53 @@ rayonM = 5
 DXV = 8
 DYM = -8
 
-Mafenetre = Tk()
-Mafenetre.title('Space invaders')
-Mafenetre.geometry('1200x800+100+50')
 
-Boutons = Frame(Mafenetre, width = 100, height = 700)
+
+Mafenetre = Tk()   # création de la fenêtre Tkinter
+Mafenetre.title('Space invaders')
+Mafenetre.geometry('1200x800+100+50') # taille et emplacement de la fenêtre
+
+Boutons = Frame(Mafenetre, width = 100, height = 700) # menu avec boutons et labels à gauche de la fenêtre
 Boutons.pack (side = 'left', padx = 5, pady = 5)
 
-BoutonQuitter = Button (Boutons, text = 'Quitter', command = Mafenetre.destroy, relief = 'groove')
+BoutonQuitter = Button (Boutons, text = 'Quitter', command = Mafenetre.destroy, relief = 'groove') # création du bouton quitter
 BoutonQuitter.pack (side = 'bottom', padx = 5, pady = 10)
+defaite = StringVar()   # création du label affichant l'issue de la partie (défaite ou victoire)
+defaite.set("")
+Defaite = Label(Boutons, textvariable = defaite, bg = 'white', fg = 'black', font = ('Arial', 30))
+Defaite.pack (side = 'top', padx = 2, pady = 0)
 
-scoretk = IntVar ()
+scoretk = IntVar ()  # création du label affichant le score
 scoreaffiche = Label(Boutons, textvariable = scoretk, bg = 'black', fg = 'yellow')
 scoreaffiche.pack (side = 'top', padx = 5, pady = 10)
 scoretk.set(score)
 
-BoutonLancer = Button (Boutons, text = 'Commencer', command = lambda:fonctioncommencer(), relief = 'groove')
+BoutonLancer = Button (Boutons, text = 'Commencer', command = lambda:fonctioncommencer(), relief = 'groove') # création du bouton de lancement du jeu (non développé)
 BoutonLancer.pack (side = 'top', padx = 5, pady = 10)
 
-MenuBar = Menu (Mafenetre)
+MenuBar = Menu (Mafenetre)  # création du menu en haut de la fenêtre (ne contient que le bouton quitter)
 Apropos = Menu (MenuBar, tearoff = 0 )
 Apropos.add_command (label = 'Quitter', command = Mafenetre.destroy)
 MenuBar.add_cascade (label = 'A propos', menu = Apropos)
 
 Mafenetre.config (menu = MenuBar)
 
-Jeu = Canvas (Mafenetre, width = largeur, height = hauteur, bg = 'white')
+Jeu = Canvas (Mafenetre, width = largeur, height = hauteur, bg = 'white') # création du canvas (zone de jeu)
 Jeu.pack (side = 'right', padx = 50, pady = 5)
 
 
-X,Y,A_liste = SIF.PositionsInit (largeur, hauteur, X, Y, Jeu, Mafenetre, rayonA)
+X,Y,A_liste = SIF.PositionsInit (largeur, hauteur, X, Y, Jeu, Mafenetre, rayonA) # initialisation de la position des aliens
 
 
-for a in A_liste:
-    a.deplacement( rayonA)
-V = SIF.Vaisseau (Jeu, Mafenetre, rayonV, rayonM, A_liste)
-Jeu.bind("<Key>", lambda event: V.deplacement(DXV, DYM, event))
+for a in A_liste:  # appel des fonctions de déplacement des aliens (vertical et horizontal)
+    a.deplacement_horizontal()
+    a.deplacement_vertical(hauteur, largeur, defaite, Defaite)
+
+V = SIF.Vaisseau (Jeu, Mafenetre, rayonV, rayonM, A_liste) # création d'un objet de la classe Vaisseau 
+
+Jeu.bind("<Key>", lambda event: V.deplacement(DXV, DYM, event, defaite, Defaite)) # interaction clavier qui déclenche les actions liées au vaisseau
 Jeu.focus_set()
 
 
 
-Mafenetre.mainloop()
+Mafenetre.mainloop() 
